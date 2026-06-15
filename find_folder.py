aedc8989e212b3e481410661bab7bda584bba756
Gangster Duck <vakopan686@gmail.com>
2026-06-15 06:25:42 +0300

    Добавлен файл types_files.py со всеми расширениями для удобства разработки

--- find_folder.py
+++ find_folder.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-def FindFolders(folder_in, folder_type):
-    folder_in = Path(folder_in)
-    for file in folder_in.rglob(folder_type):
-        return file
+def FindFileFromFolder(folder_in, folder_type):
+    for file in Path(folder_in).rglob(folder_type):
+        print(file)
---
+++ types_files.py
@@ -0,0 +1,20 @@
+types = {
+    "photo" : ("*.jpg", "*.png", "*.webr", "*.jpeg", "*.bmp", "*.gif"),
+
+    "video" : ("*.mp4", "*.avi", "*.mov", "*.mkv", "*.wmv", "*.flv",
+               "*.webm", "*.m4v", "*.mpg", "*.mpeg", "*.3gp", "*.3g2",
+               "*.mts", "*.m2ts", "*.ts", "*.vob", "*.ogv", "*.divx",
+               "*.f4v", "*.rm", "*.rmvb", "*.asf", "*.swf"),
+
+    "text" : ("*.txt", "*.doc", "*.docx", "*.pdf", "*.rtf", "*.odt",
+              "*.md", "*.tex", "*.wpd", "*.pages", "*.xml", "*.html",
+              "*.htm", "*.csv", "*.xls", "*.xlsx", "*.ppt", "*.pptx",
+              "*.epub", "*.fb2", "*.mobi", "*.azw", "*.azw3", "*.ibooks",
+              "*.lit", "*.pdb", "*.prc", "*.cbr", "*.cbz"),
+
+    "music" : ("*.mp3", "*.wav", "*.flac", "*.aac", "*.ogg", "*.m4a", "*.wma",
+               "*.opus", "*.ape", "*.alac", "*.aiff", "*.au", "*.mid", "*.midi",
+               "*.kar", "*.mod", "*.it", "*.xm", "*.s3m", "*.mtm", "*.umx", "*.ra",
+               "*.ram", "*.dss", "*.msv", "*.dvf", "*.ivr", "*.m4p", "*.ac3", "*.dts",
+               "*.mka", "*.tak", "*.tta", "*.wv", "*.voc", "*.vox", "*.gsm")
+}
